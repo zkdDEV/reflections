91c8926e03a036576c5f18aeeae26e5580f4c98f
zkd <matheus.reichemback.stang@gmail.com>
2024-06-18 18:52:45 -0300

    feat: feito o código de reflection

--- src/ClasseComAnnotation.java
+++ src/ClasseComAnnotation.java
@@ -1,6 +1,19 @@
 
-public class ClasseComAnnotation extends ClasseComAnnotationAbstrata {
-    public ClasseComAnnotation(String name){
-        super(name);
+@Tabela(value = "Tabela de Classe")
+public class ClasseComAnnotation {
+    @Tabela(value = "Tabela de Propriedade")
+    private String name;
+
+    @Tabela(value = "Tabela de Construtor")
+    public ClasseComAnnotation(){
+    }
+    @Tabela(value = "Tabela de Método sem parâmetro")
+    public String getName() {
+        return name;
+    }
+
+    @Tabela(value = "Tabela de Método com parâmetro")
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/Demo.java
+++ src/Demo.java
@@ -1,8 +1,53 @@
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class Demo {
 
     public static void main(String[] args){
-        ClasseComAnnotation object = new ClasseComAnnotation("Matheus");
-        System.out.println(object.getName());
+        ClasseComAnnotation object = new ClasseComAnnotation();
+
+        System.out.println("*******************************_Classe_********************************");
+        if(object.getClass().isAnnotationPresent(Tabela.class)){
+            Tabela annotation = object.getClass().getAnnotation(Tabela.class);
+            System.out.println("A Classe '" + object.getClass().getSimpleName() + "' contém a annotation 'Tabela' com o valor: " + annotation.value());
+        }
+
+        System.out.println("");
+
+        Field[] campos = object.getClass().getDeclaredFields();
+        System.out.println("*******************************_Propriedades_********************************");
+        for(Field campo : campos){
+            if(campo.isAnnotationPresent(Tabela.class)){
+                Tabela annotation = campo.getAnnotation(Tabela.class);
+                System.out.println("A Propriedade '" + campo.getName() + "' contém a annotation 'Tabela' com o valor: " + annotation.value());
+            }
+        }
+
+        System.out.println("");
+
+        System.out.println("*******************************_Construtor_********************************");
+        try {
+            Constructor<?> constructor = object.getClass().getDeclaredConstructor();
+            if(constructor.isAnnotationPresent(Tabela.class)){
+                Tabela annotation = (Tabela) constructor.getAnnotation(Tabela.class);
+                System.out.println("A Construtor '" + constructor.getName() + "' contém a annotation 'Tabela' com o valor: " + annotation.value());
+            }
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("");
+
+        System.out.println("*******************************_Métodos_********************************");
+        Method[] methods = object.getClass().getDeclaredMethods();
+        for (Method method : methods){
+            if(method.isAnnotationPresent(Tabela.class)){
+                Tabela annotation = method.getAnnotation(Tabela.class);
+                System.out.println("O Método '" + method.getName() + "' contém a annotation 'Tabela' com o valor: " + annotation.value());
+            }
+        }
 
     }
 }
